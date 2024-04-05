665ab8cb5310642f9d6a9763fd57ce19f2b1f925
devPxT <gabrielkauasantos11@gmail.com>
2024-04-05 09:20:00 -0300

    atv 05 part 2 em sala

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
@@ -59,5 +61,181 @@ public class Main {
         CBR.comprarMoto(vendedor, clienteValido);
 
         clienteValido.imprimirEndereco();
+
+        // System.out.println("---------- Bem Vindo ---------");
+        // System.out.println("1. Cadastrar Usuário");
+        // System.out.println("2. Listar Usuário");
+        // System.out.println("3. Cadastrar Automovel");
+        // System.out.println("4. Listar Automoveis");
+        // System.out.println("5. Adicionar Saldo");
+        // System.out.println("6. Comprar Automovel");
+        // System.out.println("0. Sair");
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.print("Digite a opção: " );
+        // int opcao = scanner.nextInt();
+        // scanner.close();
+
+        // System.out.println(opcao);
+
+        showMenu();
+    }
+
+    public static void showMenu() {
+        Scanner scanner = new Scanner(System.in);
+        int opcao = -1;
+
+        while (opcao != 0) {
+            System.out.println("---------- Bem Vindo ---------");
+            System.out.println("1. Cadastrar Usuário");
+            System.out.println("2. Listar Usuários");
+            System.out.println("3. Cadastrar Automovel");
+            System.out.println("4. Listar Automoveis");
+            System.out.println("5. Gerenciar Saldo");
+            System.out.println("6. Comprar Automovel");
+            System.out.println("0. Sair");
+            System.out.print("Digite a opção: ");
+
+            try {
+                opcao = scanner.nextInt();
+                switch (opcao) {
+                    case 1:
+                        System.out.println("Opção 1 selecionada - Cadastrar Usuário");
+                        showCadastro();
+                        break;
+                    case 2:
+                        System.out.println("Opção 2 selecionada - Listar Usuário");
+                        break;
+                    case 3:
+                        System.out.println("Opção 3 selecionada - Cadastrar Automovel");
+                        showCadastroAuto();
+                        break;
+                    case 4:
+                        System.out.println("Opção 4 selecionada - Listar Automoveis");
+                        break;
+                    case 5:
+                        System.out.println("Opção 5 selecionada - Gerenciar Saldo");
+                        showSaldo();
+                        break;
+                    case 6:
+                        System.out.println("Opção 6 selecionada - Comprar Automovel");
+                        break;
+                    case 0:
+                        System.out.println("Encerrando o programa...");
+                        break;
+                    default:
+                        System.out.println("Opção inválida. Por favor, escolha uma opção válida.");
+                }
+            } catch (Exception e) {
+                System.out.println("Erro de entrada. Por favor, digite um número válido.");
+            }
+        }
+
+        scanner.close();
+    }
+
+    public static void showCadastro() {
+        Scanner scanner = new Scanner(System.in);
+        int opcao = -1;
+
+        while (opcao != 0) {
+            System.out.println("---------- Cadastro de Us ---------");
+            System.out.println("1. Cadastrar Vendedor");
+            System.out.println("2. Cadastrar Cliente");
+            System.out.println("0. Voltar");
+            System.out.print("Digite a opção: ");
+
+            try {
+                opcao = scanner.nextInt();
+                switch (opcao) {
+                    case 1:
+                        System.out.println("Opção 1 selecionada - Cadastrar Vendedor");
+                        break;
+                    case 2:
+                        System.out.println("Opção 2 selecionada - Cadastrar Cliente");
+                        break;
+                    case 0:
+                        System.out.println("Voltando...");
+                        break;
+                    default:
+                        System.out.println("Opção inválida. Por favor, escolha uma opção válida.");
+                }
+            } catch (Exception e) {
+                System.out.println("Erro de entrada. Por favor, digite um número válido.");
+            }
+        }
+    }
+
+    public static void showCadastroAuto() {
+        Scanner scanner = new Scanner(System.in);
+        int opcao = -1;
+
+        while (opcao != 0) {
+            System.out.println("---------- Cdastro de Automóveis ---------");
+            System.out.println("1. Cadastrar Carro");
+            System.out.println("2. Cadastrar Moto");
+            System.out.println("3. Cadastrar Caminhão");
+            System.out.println("0. Voltar");
+            System.out.print("Digite a opção: ");
+
+            try {
+                opcao = scanner.nextInt();
+                switch (opcao) {
+                    case 1:
+                        System.out.println("Opção 1 selecionada - Cadastrar Carro");
+                        break;
+                    case 2:
+                        System.out.println("Opção 2 selecionada - Cadastrar Moto");
+                        break;
+                    case 3:
+                        System.out.println("Opção 3 selecionada - Cadastrar Caminhão");
+                        break;
+                    case 0:
+                        System.out.println("Voltando...");
+                        break;
+                    default:
+                        System.out.println("Opção inválida. Por favor, escolha uma opção válida.");
+                }
+            } catch (Exception e) {
+                System.out.println("Erro de entrada. Por favor, digite um número válido.");
+                scanner.nextLine(); // tira os valores do scanner
+            }
+        }
+
+        scanner.close();
+    }
+
+    public static void showSaldo() {
+        Scanner scanner = new Scanner(System.in);
+        int opcao = -1;
+
+        while (opcao != 0) {
+            System.out.println("---------- Gerenciar Saldo ---------");
+            System.out.println("1. Adicionar Saldo");
+            System.out.println("2. Remover Saldo");
+            System.out.println("0. Voltar");
+            System.out.print("Digite a opção: ");
+
+            try {
+                opcao = scanner.nextInt();
+                switch (opcao) {
+                    case 1:
+                        System.out.println("Opção 1 selecionada - Adicionar Saldo");
+                        break;
+                    case 2:
+                        System.out.println("Opção 2 selecionada - Remover Saldo");
+                        break;
+                    case 0:
+                        System.out.println("Voltando...");
+                        break;
+                    default:
+                        System.out.println("Opção inválida. Por favor, escolha uma opção válida.");
+                }
+            } catch (Exception e) {
+                System.out.println("Erro de entrada. Por favor, digite um número válido.");
+                scanner.nextLine(); // tira os valores do scanner
+            }
+        }
+
+        scanner.close();
     }
 }
